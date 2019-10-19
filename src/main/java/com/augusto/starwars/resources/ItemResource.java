82c67f3180f40bc320046edc36e38fd9d82a5efc
Augusto Guimarães <guguinha7@gmail.com>
2019-10-19 13:37:14 -0300

    Add Operações de CRUD para Item

--- src/main/java/com/augusto/starwars/resources/ItemResource.java
+++ src/main/java/com/augusto/starwars/resources/ItemResource.java
@@ -1,11 +1,16 @@
 package com.augusto.starwars.resources;
 
+import java.net.URI;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.augusto.starwars.domain.Item;
 import com.augusto.starwars.services.ItemService;
@@ -24,4 +29,32 @@ public class ItemResource {
 	
 		return ResponseEntity.ok().body(obj);
 	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Item obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody Item obj, @PathVariable Integer id){
+		obj.setId(id); // para garantir que o update seja realizado no Item correto
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<Item>> findAll() {
+		List<Item> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
 }
--- src/main/java/com/augusto/starwars/services/ItemService.java
+++ src/main/java/com/augusto/starwars/services/ItemService.java
@@ -1,5 +1,6 @@
 package com.augusto.starwars.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +21,25 @@ public class ItemService {
 		
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Item.class.getName()));
+	}
+	
+	public Item insert(Item item) {
+		item.setId(null); /* para garantir a inserção um novo item */
+		return repo.save(item);
+	}
+	
+	public Item update(Item item) {
+		find(item.getId()); /* caso o id não exista o find já gera uma exceção, pois não se pode dar um update se o item não existe. */
+		return repo.save(item);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		repo.deleteById(id);
+	}
+	
+	public List<Item> findAll(){
+		return repo.findAll();
 	}
 	
 }
